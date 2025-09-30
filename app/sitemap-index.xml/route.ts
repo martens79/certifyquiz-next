7266c8e8fab74568c581e0958983a677c2568ed8
Lorenzo Beux <lorenzobeux@gmail.com>
2025-09-30 18:35:32 +0200

    feat(sitemap): aggiunto x-default hreflang su tutte le certificazioni

--- app/sitemap-index.xml/route.ts
+++ app/sitemap-index.xml/route.ts
@@ -49,18 +49,20 @@ export async function GET() {
 
   // Dettagli con hreflang
   for (const slug of slugs) {
-    const map = Object.fromEntries(langs.map(l => [l, `${site}/${l}/${base[l]}/${slug}`]));
-    for (const l of langs) {
-      urls.push(`
-        <url>
-          <loc>${map[l]}</loc>
-          <lastmod>${now}</lastmod>
-          <changefreq>weekly</changefreq>
-          <priority>0.7</priority>
-          ${langs.map(x => `<xhtml:link rel="alternate" hreflang="${x}" href="${map[x]}"/>`).join("\n")}
-        </url>`);
-    }
+  const map = Object.fromEntries(langs.map(l => [l, `${site}/${l}/${base[l]}/${slug}`]));
+  for (const l of langs) {
+    urls.push(`
+      <url>
+        <loc>${map[l]}</loc>
+        <lastmod>${now}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+        ${langs.map(x => `<xhtml:link rel="alternate" hreflang="${x}" href="${map[x]}"/>`).join("\n")}
+        <xhtml:link rel="alternate" hreflang="x-default" href="${map.it}"/>
+      </url>`);
   }
+}
+
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
