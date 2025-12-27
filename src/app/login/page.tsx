ec3eed1001ef4a8c8adf4107293953ffad54cb14
Lorenzo Beux <lorenzobeux@gmail.com>
2025-12-27 01:05:19 +0100

    fix: pass initialLang to login EN root page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,5 @@
-// src/app/login/page.tsx
 import LoginClient from "@/app/[lang]/login/login-client";
 
 export default function LoginEN() {
-  return <LoginClient lang="en" />;
+  return <LoginClient initialLang="en" />;
 }
