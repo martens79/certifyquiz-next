26b0fe847ba8d86afdafa8a4338a3d62f5699e31
Lorenzo Beux <lorenzobeux@gmail.com>
2026-06-09 23:58:20 +0200

    login sporca la console con troppi redirect messo follow true

--- src/app/[lang]/login/page.tsx
+++ src/app/[lang]/login/page.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata(
   return {
     title,
     description,
-    robots: { index: false, follow: false }, // 👈 importantissimo
+    robots: { index: false, follow: true }, // 👈 importantissimo
     openGraph: {
       title,
       description,
