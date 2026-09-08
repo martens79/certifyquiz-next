959fbdcafc0735ced82326dd35dffd0e5844e049
Lorenzo Beux <lorenzobeux@gmail.com>
2026-09-08 20:29:51 +0200

    Fix English blog metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,18 @@
 // src/app/blog/[slug]/page.tsx
-import BlogArticleLangPage from "@/app/[lang]/blog/[slug]/page";
+import BlogArticleLangPage, {
+  generateMetadata as generateLangMetadata,
+} from "@/app/[lang]/blog/[slug]/page";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  return generateLangMetadata({
+    params: Promise.resolve({ lang: "en", slug }),
+  });
+}
 
 export default async function BlogArticlePage({
   params,
